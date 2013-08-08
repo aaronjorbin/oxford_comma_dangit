a7ccf5015024ea822b8e2dcfaf6157d95f5afbec
Aaron Jorbin <aaron@jorb.in>
2013-08-08 00:59:47 -0400

    add initial unit tests

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -1,10 +1,31 @@
 <?php
 
-class SampleTest extends WP_UnitTestCase {
+class OxfordCommaDangitTest extends WP_UnitTestCase {
 
-	function testSample() {
-		// replace this with some actual testing code
-		$this->assertTrue( true );
+	function testStrippers() {
+		$string      = 'We invited the strippers, JFK and Stalin.';
+		$commaString = 'We invited the strippers, JFK, and Stalin.';
+		$newString   = oxford_comma_dangit($string);
+		$newString2  = oxford_comma_dangit($string);
+
+		$this->assertSame($commaString, $newString );
+		$this->assertSame($commaString, $newString2 );
+	}
+
+	function testNoComma(){
+		$string    = 'Scott Taylor says YOLO and Balls';
+		$newString =  oxford_comma_dangit($string);
+		$this->assertSame($string, $newString);
+	}
+
+	function testPhrase(){
+		$string      = 'WordPress is a content management system, application framework and blogging platform.';
+		$commaString = 'WordPress is a content management system, application framework, and blogging platform.';
+		$newString   = oxford_comma_dangit($string);
+		$newString2  = oxford_comma_dangit($commaString);
+
+		$this->assertSame($commaString, $newString );
+		$this->assertSame($commaString, $newString2 );
 	}
 }
 
